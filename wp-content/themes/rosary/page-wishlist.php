a1fc598b39538069dc2dc6bbc76d03e0fa3140d0
Wecan Company <contacto@wecan.company>
2020-11-30 19:53:03 +0000

    Single mobile

--- wp-content/themes/rosary/page-wishlist.php
+++ wp-content/themes/rosary/page-wishlist.php
@@ -1,3 +1,10 @@
 <?php get_header(); ?>
 <?php  echo do_shortcode('[yith_wcwl_wishlist]'); ?> 
+<style>
+	@media (max-width: 767px) and (min-width: 0px){
+.woocommerce {
+    margin-top: 0!important;
+}
+	}
+</style>
 <?php get_footer(); ?>
