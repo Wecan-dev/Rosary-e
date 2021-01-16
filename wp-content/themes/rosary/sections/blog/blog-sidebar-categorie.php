f510e793e1e917decd4ab48b6d157a66cea28d8a
Wecan Company <contacto@wecan.company>
2021-01-16 03:32:03 +0000

    Issues qa client

--- wp-content/themes/rosary/sections/blog/blog-sidebar-categorie.php
+++ wp-content/themes/rosary/sections/blog/blog-sidebar-categorie.php
@@ -20,3 +20,38 @@
               <?php endforeach; ?>  
             </ul>
           </div>
+
+
+          <div class="blog-sidebar__content">
+            <p class="blog-sidebar__title">
+              Colecciones
+            </p>
+		
+			  
+			  
+        <ul class="blog-sidebar__categorie">
+                  <?php
+          global $wpdb;
+          $product_categories = get_categories( array( 'taxonomy' => 'product_cat', 'orderby' => 'menu_order', 'order' => 'asc'  ));  
+          ?>                                                        
+          <?php foreach($product_categories as $category): ?>
+            <?php $checked =NULL;  if ($category->slug == $_GET['cat']) { $checked = "checked='checked'"; } $categoria = $category->name; $category_id = $category->term_id; $category_link = get_category_link( $category_id ); ?> 
+            <?php if($category->parent >1){ ?>  
+              <li>
+                <a href="<?php echo get_category_link( $category->term_id ); ?>">
+                   <?= $categoria ?>
+                </a>
+              </li>
+            <?php } ?>  
+              <?php endforeach; ?>  
+            </ul>
+          </div>
+
+
+
+
+
+
+
+
+
